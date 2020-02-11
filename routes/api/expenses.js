12cc06688e1f78be6d12af17a77e7997a1bad3f6
Mateusz Makowski <matmakos@gmail.com>
2020-02-11 23:41:04 +0100

    Add POST to routes

--- routes/api/expenses.js
+++ routes/api/expenses.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../../models');
-const { Op } = require('sequelize');
+const {Op} = require('sequelize');
 
 router.get('/', function(req, res, next) {
-  let searchClause = { where: { RecordType: 'Expense' } };
+  const searchClause = {where: {RecordType: 'Expense'}};
 
   // /api/expenses?limit=[INTEGER] to limit number of returned records.
   if (req.query.limit) {
@@ -21,7 +21,7 @@ router.get('/', function(req, res, next) {
     const dateFrom = new Date(req.query.dateFrom);
     const dateTo = new Date(req.query.dateTo);
 
-    if (dateFrom == "Invalid Date" || dateTo == "Invalid Date") {
+    if (dateFrom == 'Invalid Date' || dateTo == 'Invalid Date') {
       return res.status(404).json({'msg': 'wrong query'});
     }
 
@@ -50,6 +50,29 @@ router.get('/:id', function(req, res, next) {
       return res.status(200).json(record);
     }
   });
+});
+
+router.post('/', function(req, res, next) {
+  if (req.body.amount) {
+    if (!req.body.date) {
+      req.body.date = new Date();
+    }
+
+    models.Record.create({
+      Amount: req.body.amount,
+      Date: req.body.date,
+      Type: req.body.type,
+      Comment: req.body.comment,
+      RecordType: 'Expense',
+      UserName: 'makos', // TODO: req.user from JWT
+    }).then((record) => {
+      res.status(200).json({'created': record});
+    }, (err) => {
+      res.status(500).json({'error': err});
+    });
+  } else {
+    res.status(400).json({'error': 'bad request'});
+  }
 });
 
 module.exports = router;
--- routes/api/income.js
+++ routes/api/income.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../../models');
-const { Op } = require('sequelize');
+const {Op} = require('sequelize');
 
 router.get('/', function(req, res, next) {
-  let searchClause = { where: { RecordType: 'Income' } };
+  const searchClause = {where: {RecordType: 'Income'}};
 
   // /api/income?limit=[INTEGER] to limit number of returned records.
   if (req.query.limit) {
@@ -21,7 +21,7 @@ router.get('/', function(req, res, next) {
     const dateFrom = new Date(req.query.dateFrom);
     const dateTo = new Date(req.query.dateTo);
 
-    if (dateFrom == "Invalid Date" || dateTo == "Invalid Date") {
+    if (dateFrom == 'Invalid Date' || dateTo == 'Invalid Date') {
       return res.status(404).json({'msg': 'wrong query'});
     }
 
@@ -53,7 +53,26 @@ router.get('/:id', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  res.status(200);
+  if (req.body.amount) {
+    if (!req.body.date) {
+      req.body.date = new Date();
+    }
+
+    models.Record.create({
+      Amount: req.body.amount,
+      Date: req.body.date,
+      Type: req.body.type,
+      Comment: req.body.comment,
+      RecordType: 'Income',
+      UserName: 'makos', // TODO: req.user from JWT
+    }).then((record) => {
+      res.status(200).json({'created': record});
+    }, (err) => {
+      res.status(500).json({'error': err});
+    });
+  } else {
+    res.status(400).json({'error': 'bad request'});
+  }
 });
 
 module.exports = router;
