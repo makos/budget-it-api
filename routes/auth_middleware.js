c614be9e3f3998c26e64f9f7ccff0ab61f1bc919
Mateusz Makowski <matmakos@gmail.com>
2020-02-13 14:23:51 +0100

    Add expiration date to JWT

--- routes/auth_middleware.js
+++ routes/auth_middleware.js
@@ -73,9 +73,9 @@ const loginUser = function(req, res, next) {
     if (user) {
       bcrypt.compare(req.body.password, user.Password, function(err, login) {
         if (login) {
-          // TODO: Expire the token in a week or something like that.
-          // Refresh it every time user passes a token check on other routes.
-          const token = jwt.sign({loggedInAs: user.Name}, config.secret);
+          const token = jwt.sign({loggedInAs: user.Name}, config.secret, {
+            expiresIn: config.jwtExp,
+          });
           return res.status(200).json(token);
         } else {
           return res.status(400).json({'Error': 'Wrong username or password.'});
