3d97d8c2d583241e4e8d6ff1a2d12e0527f31bbe
Mateusz Makowski <matmakos@gmail.com>
2020-02-17 20:22:01 +0100

    Remove unused packages

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const recordsRouter = require('./routes/api/records');
@@ -12,9 +10,6 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(['/api/income', '/api/expenses'], recordsRouter);
 app.use('/api/login', loginRouter);
